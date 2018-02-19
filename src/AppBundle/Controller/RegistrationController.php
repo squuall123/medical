3feeece6cccaa8faa369d1c32cf15f45ab1f7da2
Arthur Amarth <squuall123@gmail.com>
2018-02-19 22:13:14 +0100

    fixed admin entity

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -7,6 +7,8 @@ use AppBundle\Entity\Patient;
 use AppBundle\Entity\Medecin;
 use AppBundle\Form\PatientType;
 use AppBundle\Form\MedecinType;
+use AppBundle\Entity\Admin;
+use AppBundle\Form\AdminType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +18,7 @@ class RegistrationController extends Controller
     /**
      * @Route("/registerMedecin", name="registerMedecin")
      */
-    public function registerAction(Request $request)
+    public function registerMedecinAction(Request $request)
     {
         // Create a new blank user and process the form
         $user = new Medecin();
@@ -32,7 +34,7 @@ class RegistrationController extends Controller
             // Set their role
             $user->setRole('ROLE_MEDECIN');
             $user->setEtat(false);
-            
+
             // Save
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
@@ -64,6 +66,40 @@ class RegistrationController extends Controller
 
             // Set their role
             $user->setRole('ROLE_PATIENT');
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('auth/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/registerAdmin", name="registerAdmin")
+     */
+    public function registerAdminAction(Request $request)
+    {
+        // Create a new blank user and process the form
+        $user = new Admin();
+        $form = $this->createForm(AdminType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $encoder = $this->get('security.password_encoder');
+            $password = $encoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // Set their role
+            $user->setRole('ROLE_ADMIN');
+            
 
             // Save
             $em = $this->getDoctrine()->getManager();
