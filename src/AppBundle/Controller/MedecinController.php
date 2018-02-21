1b4688327092c1564d6e7563c66c4a306ac68894
Arthur Amarth <squuall123@gmail.com>
2018-02-21 10:29:49 +0100

    added RDV

---
+++ src/AppBundle/Controller/MedecinController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Medecin;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Medecin controller.
+ *
+ * @Route("medecin")
+ */
+class MedecinController extends Controller
+{
+    /**
+     * Lists all medecin entities.
+     *
+     * @Route("/", name="medecin_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $medecins = $em->getRepository('AppBundle:Medecin')->findAll();
+
+        return $this->render('medecin/index.html.twig', array(
+            'medecins' => $medecins,
+        ));
+    }
+
+    /**
+     * Finds and displays a medecin entity.
+     *
+     * @Route("/{id}", name="medecin_show")
+     * @Method("GET")
+     */
+    public function showAction(Medecin $medecin)
+    {
+
+        return $this->render('medecin/show.html.twig', array(
+            'medecin' => $medecin,
+        ));
+    }
+}
